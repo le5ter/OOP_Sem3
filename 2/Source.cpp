3ce4155474255c5cce54ddae78d485d7740463c1
Alexey <al.soshnikov@icloud.com>
2023-08-12 09:29:11 +0300

    Lab 2v1

--- 2/Source.cpp
+++ 2/Source.cpp
@@ -1,9 +1,10 @@
-//Создать класс Complex комплексных чисел поля класса указатели на тип double
+//Создать класс Complex комплексных чисел. Поля класса указатели на тип double.
 #include "Complex.h"
 
 using namespace std;
 
 int main() {
+	system("chcp 1251");
 	Complex A(1, 2);
 	Complex B(2, 3);
 	Complex C;
